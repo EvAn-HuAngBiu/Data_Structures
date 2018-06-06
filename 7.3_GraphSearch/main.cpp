6a0b51fc852164d42033c9cf98f74ca24472dcbe
Evan <arthurh9802@gmail.com>
2018-06-06 21:35:22 +0800

    Init graph storage

---
+++ 7.3_GraphSearch/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "ALGraph.h"
+using namespace std;
+
+int main() {
+    ALGraph graph(8, UDG, new string[8]{"v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8"});
+    graph.InsertArc(0, 2);
+    graph.InsertArc(0, 1);
+    graph.InsertArc(1, 4);
+    graph.InsertArc(1, 3);
+    graph.InsertArc(2, 6);
+    graph.InsertArc(2, 5);
+    graph.InsertArc(3, 7);
+    graph.InsertArc(4, 7);
+    graph.InsertArc(5, 6);
+    graph.DFSTraverse();
+    cout << endl;
+    graph.BFSTraverse();
+    cout << endl;
+
+    return 0;
+}
